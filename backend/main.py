91995ef34ce07064282e7968166a9c7d9664d5ff
arun ekambaram <arunvit1730@gmail.com>
2025-04-05 17:27:42 +0530

    Connected Backend to Frontend

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# 👇 Allow cross-origin requests (important for frontend access)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
     return{"message": "Welcome to Materialslab API"}
+
+
